9b0f03698a91ddffb3c1278fa426c77ddaa7a5d0
Jared Phillips <jaredtphillips@me.com>
2013-07-04 12:09:43 -0400

    added view counter to main page, reseting it when adding a item to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,20 @@ class StoreController < ApplicationController
 
   def index
   	@products = Product.order(:title)
+  	@count = counter
   end
+
+  def reset_counter
+  	@count = session[:session] = 0 
+  end
+
+  def counter
+  	# if session[:counter].nil? session[:counter] = 0 : session[:counter] += 1
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	else
+  		session[:counter] += 1
+  	end
+  end
+
 end
